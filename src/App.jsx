01a9e37a168a78cbd5bd564b1b7d9ead2010f705
Fransvis <francois.haf@gmail.com>
2020-10-23 12:14:55 +0200

    Fransvis: added NavBrand and Navlinks Component, but still need to configure properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import Brand from './Components/Navbar/brand.jsx';
+import Navlink from './Components/Navbar/links.jsx';
 
+
+
+const name = 'Frans Visser';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Brand />
+      <Navlink />
+      <h1>Hello {name}</h1>
     </div>
   );
 }
---
+++ src/Components/Navbar/links.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Navlinks extends Component {
+  state = {  }
+  render() { 
+    return ( <li class="nav-item">
+    <a class="nav-link active" href="#">Active</a>
+  </li>);
+  }
+}
+ 
+export default Navlinks;
